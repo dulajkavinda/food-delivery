e5e3276833500810695cfe16c1ac528bc3b84880
Dulaj Kavinda <dulajkaavinda@gmail.com>
2020-12-23 21:51:17 +0530

    restuarant grid completed

--- lib/screens/restuarant_screen.dart
+++ lib/screens/restuarant_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:food_delivery_FT/models/food.dart';
 import 'package:food_delivery_FT/models/restaurant.dart';
 import 'package:food_delivery_FT/widgets/rating_stars.dart';
 
@@ -69,6 +70,9 @@ class _RestuarantScreenState extends State<RestuarantScreen> {
                     )
                   ],
                 ),
+                SizedBox(
+                  height: 2.0,
+                ),
                 RatingStars(rating: widget.restaurant.rating),
                 SizedBox(
                   height: 6.0,
@@ -80,6 +84,140 @@ class _RestuarantScreenState extends State<RestuarantScreen> {
                   ),
                 )
               ],
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              FlatButton(
+                onPressed: () {},
+                child: Text(
+                  'Reviews',
+                  style: TextStyle(fontSize: 20.0, color: Colors.white),
+                ),
+                padding: EdgeInsets.symmetric(horizontal: 30.0),
+                color: Theme.of(context).primaryColor,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10.0)),
+              ),
+              FlatButton(
+                onPressed: () {},
+                child: Text(
+                  'Contact',
+                  style: TextStyle(fontSize: 20.0, color: Colors.white),
+                ),
+                padding: EdgeInsets.symmetric(horizontal: 30.0),
+                color: Theme.of(context).primaryColor,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10.0)),
+              )
+            ],
+          ),
+          SizedBox(
+            height: 10.0,
+          ),
+          Center(
+            child: Text(
+              'Menu',
+              style: TextStyle(
+                  fontSize: 22.0,
+                  fontWeight: FontWeight.w600,
+                  letterSpacing: 1.2),
+            ),
+          ),
+          SizedBox(
+            height: 10.0,
+          ),
+          Expanded(
+            child: GridView.count(
+              padding: EdgeInsets.all(10.0),
+              crossAxisCount: 2,
+              children: List.generate(widget.restaurant.menu.length, (index) {
+                Food food = widget.restaurant.menu[index];
+                return Center(
+                  child: _buildMenuItem(food),
+                );
+              }),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _buildMenuItem(Food food) {
+    return Center(
+      child: Stack(
+        alignment: Alignment.center,
+        children: [
+          Container(
+            height: 175.0,
+            width: 175.0,
+            decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage(food.imageUrl),
+                  fit: BoxFit.cover,
+                ),
+                borderRadius: BorderRadius.circular(15.0)),
+          ),
+          Container(
+            height: 175.0,
+            width: 175.0,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(15.0),
+              gradient: LinearGradient(
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomLeft,
+                  colors: [
+                    Colors.black.withOpacity(0.3),
+                    Colors.black87.withOpacity(0.3),
+                    Colors.black54.withOpacity(0.3),
+                    Colors.black38.withOpacity(0.3),
+                  ],
+                  stops: [
+                    0.1,
+                    0.5,
+                    0.7,
+                    0.9
+                  ]),
+            ),
+          ),
+          Positioned(
+            bottom: 65.0,
+            child: Column(
+              children: [
+                Text(
+                  food.name,
+                  style: TextStyle(
+                      fontSize: 24.0,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      letterSpacing: 1.2),
+                ),
+                Text(
+                  '\$${food.price}',
+                  style: TextStyle(
+                      fontSize: 18.0,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      letterSpacing: 1.2),
+                )
+              ],
+            ),
+          ),
+          Positioned(
+            bottom: 10.0,
+            right: 10.0,
+            child: Container(
+              child: IconButton(
+                color: Colors.white,
+                icon: Icon(Icons.add),
+                onPressed: () {},
+              ),
+              width: 48.0,
+              decoration: BoxDecoration(
+                  color: Theme.of(context).primaryColor,
+                  borderRadius: BorderRadius.circular(30.0)),
             ),
           )
         ],
