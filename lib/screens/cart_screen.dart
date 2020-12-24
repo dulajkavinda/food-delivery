43ff7aa117d0dc14494aa9b762699ddc0e22fe0b
Dulaj Kavinda <dulajkaavinda@gmail.com>
2020-12-24 06:33:44 +0530

    checkout button

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -10,6 +10,9 @@ class CartScreen extends StatefulWidget {
 class _CartScreenState extends State<CartScreen> {
   @override
   Widget build(BuildContext context) {
+    double totalPrice = 0;
+    currentUser.cart.forEach(
+        (Order order) => totalPrice += order.quantity * order.food.price);
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
@@ -20,8 +23,59 @@ class _CartScreenState extends State<CartScreen> {
       ),
       body: ListView.separated(
           itemBuilder: (context, index) {
-            Order order = currentUser.cart[index];
-            return _buildCartItem(order);
+            if (index < currentUser.cart.length) {
+              Order order = currentUser.cart[index];
+              return _buildCartItem(order);
+            }
+            return Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'Estimated Delivery Time:',
+                        style: TextStyle(
+                            fontSize: 20.0, fontWeight: FontWeight.w600),
+                      ),
+                      Text(
+                        '25 min',
+                        style: TextStyle(
+                          fontSize: 20.0,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 10.0,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'Total Cost:',
+                        style: TextStyle(
+                          fontSize: 20.0,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                      Text(
+                        '\$${totalPrice.toStringAsFixed(2)}',
+                        style: TextStyle(
+                            fontSize: 20.0,
+                            fontWeight: FontWeight.w600,
+                            color: Colors.green[700]),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 100.0,
+                  )
+                ],
+              ),
+            );
           },
           separatorBuilder: (context, index) {
             return Divider(
@@ -29,7 +83,28 @@ class _CartScreenState extends State<CartScreen> {
               color: Colors.grey,
             );
           },
-          itemCount: currentUser.cart.length),
+          itemCount: currentUser.cart.length + 1),
+      bottomSheet: Container(
+        height: 100.0,
+        decoration: BoxDecoration(
+            color: Theme.of(context).primaryColor,
+            boxShadow: [
+              BoxShadow(
+                  color: Colors.black26, offset: Offset(0, -1), blurRadius: 6.0)
+            ]),
+        child: Center(
+          child: FlatButton(
+            child: Text(
+              'CHECKOUT',
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 22.0,
+                  fontWeight: FontWeight.bold),
+            ),
+            onPressed: () {},
+          ),
+        ),
+      ),
     );
   }
 
@@ -115,7 +190,7 @@ class _CartScreenState extends State<CartScreen> {
                           ),
                         ],
                       ),
-                    )
+                    ),
                   ],
                 ),
               ),
